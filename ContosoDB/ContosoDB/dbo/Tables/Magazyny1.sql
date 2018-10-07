fbbaa31b61fbc99a4b761fa2e3f0cc884f258fcc
claudia.hopfl <claudia.hopfl@gmail.com>
2018-10-07 13:49:46 +0100

    Dodanie nowej tabeli Update

---
+++ ContosoDB/ContosoDB/dbo/Tables/Magazyny1.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Magazyny1] (
+    [Magazyny_ID]    INT           IDENTITY (1, 1) NOT NULL,
+    [Nazwa_Magazynu] VARCHAR (100) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Magazyny_ID] ASC)
+);
+
